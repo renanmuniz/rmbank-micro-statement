c36a8defac916af8b43858b60fb3c2b1ac0687e3
renanmuniz <renan.merlo@paygo.com.br>
2022-05-12 23:25:56 -0300

    Criacao do projeto

---
+++ micro-statement/src/main/java/com/rmbank/microstatement/MicroStatementApplication.java
@@ -0,0 +1,13 @@
+package com.rmbank.microstatement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroStatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroStatementApplication.class, args);
+	}
+
+}
---
+++ micro-statement/src/test/java/com/rmbank/microstatement/MicroStatementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmbank.microstatement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroStatementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
